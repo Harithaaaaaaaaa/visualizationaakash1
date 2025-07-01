3a0f2345d1cb02343317d52c5a206f74881ee389
Harithaaaaaaaaa <162455673+Harithaaaaaaaaa@users.noreply.github.com>
2025-07-01 11:00:39 +0530

    Update filterdashboard.py

--- filterdashboard.py
+++ filterdashboard.py
@@ -135,6 +135,8 @@ def update_dashboard(device_id, start_date, end_date):
     df_tags = df.groupby('device_id_id', as_index=False)['tags_count'].sum()
     df_tags = df_tags.sort_values(by='device_id_id',ascending=True)
 
+    device_order = sorted(df_tags['device_id_id'].unique(), key=int)
+
     fig_tags = px.bar(df_tags, x='device_id_id', y='tags_count', title='Tag Reads per Device')
 
     # Bar chart - Sessions
